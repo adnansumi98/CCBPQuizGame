aff2d872b344f66d0f2d44732805cffbd5c2b296
S. Mohamed Adnan <62370865+adnansumi98@users.noreply.github.com>
2024-09-09 10:17:53 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import {Switch, Route} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 import Login from './components/Login'
+import Header from './components/Header'
 
-const App = () => <Login />
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Header} />
+  </Switch>
+)
 
 export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,38 @@
+import Cookies from 'js-cookie'
+import {useHistory} from 'react-router-dom'
+import './index.css'
+
+const Header = () => {
+  const history = useHistory()
+  const handleLogout = () => {
+    Cookies.remove('jwt_token')
+    history.push('/login')
+  }
+
+  return (
+    <div className="header-container">
+      <img
+        src="/img/logo-header.png"
+        alt="website logo"
+        className="header-logo"
+      />
+      <div className="logout-container">
+        <img
+          src="/img/log-out.png"
+          alt="logout"
+          className="logout-icon"
+          onClick={handleLogout}
+        />
+        <button
+          type="button"
+          onClick={handleLogout}
+          className="logout-button desktop"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
