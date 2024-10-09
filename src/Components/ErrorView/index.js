c0b399871de4d72ba3d2319bb9f21c52e96415a5
Mohamed Adnan <adnansumi98@gmail.com>
2024-10-09 15:59:51 +0530

    changed error view file

--- src/Components/ErrorView/index.js
+++ src/Components/ErrorView/index.js
@@ -2,9 +2,13 @@ import './index.css'
 
 const ErrorView = ({onRetry}) => (
   <div className="form-container form-col">
-    <img src="img/Error.png" alt="Error" className="" />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-assess-failure-img.png"
+      alt="failure view"
+      className=""
+    />
     <h1>Something went Wrong</h1>
-    <p>Our servers are busy. Please try again</p>
+    <p>Our servers are busy please try again</p>
     <button type="button" onClick={onRetry}>
       Retry
     </button>
