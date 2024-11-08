13852230a29fd0334e4ff4af39448c1340b761d2
S. Mohamed Adnan <62370865+adnansumi98@users.noreply.github.com>
2024-11-08 16:59:14 +0530

    Update ccbp git set up.sh
    tested for working as of  November 8 2024

--- ccbp git set up.sh
+++ ccbp git set up.sh
@@ -1,12 +1,14 @@
+# reset the IDE
+### delete the existing folders
+rm -rf src
+rm -rf public
 
-# delete the existing folders
-rm -rf src, public
-
-# remove lock file 
+### remove lock file 
 rm -rf *lock*
+rm -rf node_modules
 
-# clone the repository and copy the files 
+### clone the repository and copy the files 
 git clone https://github.com/adnansumi98/CCBPQuizGame.git temp1 && mv temp1/* ../quizgame/ && mv temp1/.* ../quizgame
 
-# remove temp folder
+### remove temp folder
 rm -rf temp1
